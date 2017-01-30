af5729105e4d3b144c74d53467399d3acac7f5f1
Quezler <Quezler@me.com>
2017-01-30 09:35:29 +0100

    Register CreateFreshApiToken middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use App\Http\Middleware\AttachUserToEverySession;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CreateFreshApiToken;
 
 class Kernel extends HttpKernel
 {
@@ -36,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             AttachUserToEverySession::class,
+            CreateFreshApiToken::class,
         ],
 
         'api' => [
